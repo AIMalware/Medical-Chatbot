f24b420df3691536f9cf7656b02de7eb8892cbf2
AIMalware <yy608489@gmail.com>
2026-03-23 08:58:00 +0530

    first commit

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+
+touch .env
+touch setup.py
+touch README.md
+touch src/prompt.py
+touch app.py
+
+touch research/trails.ipynb
+touch requirements.txt
+echo  "Bro your files and directories are created succesfully"
